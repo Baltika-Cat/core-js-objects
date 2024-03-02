cdb4af9c28893c8829c67e5fda919aa119481a46
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-03-02 06:21:56 +0300

    feat: complete first task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const object = {};
+  return Object.assign(object, obj);
 }
 
 /**
@@ -32,8 +33,19 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const obj = {};
+  for (let i = 0; i < objects.length; i += 1) {
+    const pairs = Object.entries(objects[i]);
+    pairs.forEach((pair) => {
+      if (obj[pair[0]]) {
+        obj[pair[0]] += objects[i][pair[1]];
+      } else {
+        obj[pair[0]] = objects[i][pair[1]];
+      }
+    });
+  }
+  return obj;
 }
 
 /**
