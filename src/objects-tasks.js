1a357c5a54da5bbc08fcb3127be7b5efb721bdc6
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-03-02 09:13:04 +0400

    feat: complete all tasks excludes three last tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -39,9 +39,9 @@ function mergeObjects(objects) {
     const pairs = Object.entries(objects[i]);
     pairs.forEach((pair) => {
       if (obj[pair[0]]) {
-        obj[pair[0]] += objects[i][pair[1]];
+        obj[pair[0]] += objects[i][pair[0]];
       } else {
-        obj[pair[0]] = objects[i][pair[1]];
+        obj[pair[0]] = objects[i][pair[0]];
       }
     });
   }
@@ -61,8 +61,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const object = { ...obj };
+  keys.forEach((key) => {
+    delete object[key];
+  });
+  return object;
 }
 
 /**
@@ -77,8 +81,8 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -92,8 +96,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -112,8 +116,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -126,8 +130,22 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  if (Object.keys(lettersObject).length === 0) {
+    return '';
+  }
+  const max =
+    Object.values(lettersObject)
+      .flat()
+      .sort((a, b) => b - a)[0] + 1;
+  const arr = new Array(max).fill('a');
+  const pairs = Object.entries(lettersObject);
+  pairs.forEach((pair) => {
+    pair[1].forEach((index) => {
+      arr.splice(index, 1, pair[0]);
+    });
+  });
+  return arr.join('');
 }
 
 /**
@@ -144,8 +162,19 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const price = 25;
+  if (queue[0] > price) {
+    return false;
+  }
+  let money = 0;
+  while (queue.length > 0) {
+    money += queue.shift();
+    if (money < queue[0]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -161,8 +190,18 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  class Rect {
+    constructor(_width, _height) {
+      this.width = _width;
+      this.height = _height;
+    }
+
+    getArea() {
+      return this.width * this.height;
+    }
+  }
+  return new Rect(width, height);
 }
 
 /**
@@ -175,8 +214,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -190,8 +229,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+  return new proto.constructor(...values);
 }
 
 /**
